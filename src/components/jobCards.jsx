02d1386fbff2a8fe876df3061d4fc960f2737860
Logan <loganmsorensen@gmail.com>
2021-03-05 10:34:12 -0800

    pagination working

--- src/components/jobCards.jsx
+++ src/components/jobCards.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -8,46 +8,21 @@ import { jobsAPI } from "../utils/jobsAPI";
 import { setSearchResults } from "../actions/setSearchResultsActions";
 
 const JobCards = (props) => {
-
-  const [currentPageJobs, setCurrentPageJobs] = useState([]);
-  const [firstJob, setFirstJob] = useState(0)
-
-  useEffect(() => {
-// setCurrentPageJobs(props.jobs)
-if (props.jobs !== undefined) {
-  // console.log(props.jobs.length < 5)
-  if (props.jobs.length < 5) {
-    setCurrentPageJobs(props.jobs)
-    console.log(currentPageJobs)
-  } else  if (props.jobs !== undefined &&props.currentPage !== props.totalPages) {
-    setCurrentPageJobs(props.jobs.slice(firstJob,firstJob + 5))
-    console.log('HERE',props.jobs.slice(firstJob, firstJob + 5))
-    setFirstJob(firstJob + 5)
-  } else if (props.jobs!==undefined && props.currentPage === props.totalPages) {
-    setCurrentPageJobs(props.jobs.slice(firstJob,props.jobs.length))
-    
-  }
-}
-}, [props.jobs, props.currentPage])
-
-const hitAPI = () => {
-  jobsAPI()
-  .get("positions.json?")
-  .then((res) => {
-    console.log(res.data);
-    props.setSearchResults(res.data, props.fullTime);
-  })
-  .catch((err) => console.log(err));
-};
-
-// console.log('first job',firstJob)
-  // console.log(currentPageJobs)
+  const hitAPI = () => {
+    jobsAPI()
+      .get("positions.json?")
+      .then((res) => {
+        console.log(res.data);
+        props.setSearchResults(res.data, props.fullTime);
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="job-cards">
       <button onClick={hitAPI}>hit API</button>
-      {currentPageJobs !== undefined &&
-        currentPageJobs.map((job) => {
+      {props.jobs !== undefined &&
+        props.jobs[props.currentPage - 1].map((job) => {
           return (
             <Link key={job.id} to={`/listing/${job.id}`}>
               <JobCard job={job} />
@@ -59,6 +34,7 @@ const hitAPI = () => {
 };
 
 const mapStateToProps = (state) => {
+  // console.log('STATE', state)
   return {
     jobs: state.searchResultsReducer.jobs,
     currentPage: state.searchResultsReducer.currentPage,
--- src/reducers/searchResultsReducer.js
+++ src/reducers/searchResultsReducer.js
@@ -5,30 +5,55 @@ const initialState = {};
 export const searchResultsReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_SEARCH_RESULTS:
-      // console.log("reducer", Math.ceil(action.payload.length / 5));
-      // console.clear();
-      // console.log(action.payload)
+      let firstNumber = 0;
+      const pageJobs = [];
+
       if (action.fullTime === true) {
         const jobs = action.payload.filter((job) => job.type === "Full Time");
+
+        let numberOfPages = Math.ceil(jobs.length / 5);
+
+        for (let i = 0; i < numberOfPages; i++) {
+          if (i === numberOfPages - 1) {
+            pageJobs.push(jobs.slice(firstNumber, jobs.length));
+          } else {
+            pageJobs.push(jobs.slice(firstNumber, firstNumber + 5));
+            firstNumber += 5;
+          }
+        }
+
         return {
           ...state,
-          jobs: jobs,
+          jobs: pageJobs,
           currentPage: 1,
           totalPages: Math.ceil(jobs.length / 5),
         };
       } else {
+        let numberOfPages = Math.ceil(action.payload.length / 5);
+
+        for (let i = 0; i < numberOfPages; i++) {
+          if (i === numberOfPages - 1) {
+            pageJobs.push(
+              action.payload.slice(firstNumber, action.payload.length)
+            );
+          } else {
+            pageJobs.push(action.payload.slice(firstNumber, firstNumber + 5));
+            firstNumber += 5;
+          }
+        }
+
         return {
           ...state,
-          jobs: action.payload,
+          jobs: pageJobs,
           currentPage: 1,
-          totalPages: Math.ceil(action.payload.length / 5),
+          totalPages: numberOfPages,
         };
       }
-      case CHANGE_PAGE:
-        // console.log('reducer', action.payload)
-        return {
-          ...state, currentPage: action.payload
-        }
+    case CHANGE_PAGE:
+      return {
+        ...state,
+        currentPage: action.payload,
+      };
     default:
       return state;
   }
